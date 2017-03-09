1bfac6660cd7802ccf9d8e6d55b9b43ae258ac90
Kaan Genç <kaan@kaangenc.me>
2017-03-09 14:12:13 +0300

    Centralize test data, add more test images

---
+++ tests/test_base.py
@@ -0,0 +1,8 @@
+from skimage import data
+from plic import colorspace, compression
+
+
+T = 3
+TEST_SOURCE = [data.astronaut(), data.coffee(), data.chelsea(), data.rocket()]
+TEST_IMAGES = list(map(colorspace.rgb2rdgdb, TEST_SOURCE))
+TEST_ERRORS = list(map(lambda i: i - compression.interpolate(compression.downsample(i, T), i.shape), TEST_IMAGES))
--- tests/test_colorspace.py
+++ tests/test_colorspace.py
@@ -1,10 +1,11 @@
 from pytest import mark
-from skimage import data
 from plic import colorspace
+
+from .test_base import TEST_SOURCE
 
 
 class TestColorspace:
-    @mark.parametrize('image', [data.astronaut(), data.coffee()])
+    @mark.parametrize('image', TEST_SOURCE)
     def test_colorspace_roundtrip(self, image):
         """Converting from RGB to the RdGdB colorspace, then back to RGB should give the same image."""
         assert (colorspace.rdgdb2rgb(colorspace.rgb2rdgdb(image)) == image).all()
